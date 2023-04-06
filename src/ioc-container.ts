e7d674885e321d9300484cfb004d343a861bbc57
Nazar.bnm <nazar.bnm@gmail.com>
2023-04-06 20:39:29 +0300

    [ADD] IoC Container resources type

--- src/ioc-container.ts
+++ src/ioc-container.ts
@@ -1,29 +1,28 @@
 import DataResource from './resources/data.resource';
 import ClassResource from './resources/class.resource';
-import { ClassType } from './types';
 
-export class IoCContainer {
-  private _resources;
+export class IoCContainer<T extends Record<string, any>> {
+  private _resources: T;
 
   constructor() {
-    this._resources = new Map();
+    this._resources = new Map() as unknown as T;
   }
 
-  has(name: string) {
+  has<K extends keyof T>(name: K) {
     return this._resources.has(name);
   }
 
-  _resolveResourceDependency(resourceName: string, dependencyName: string) {
+  _resolveResourceDependency<K extends keyof T>(resourceName: K, dependencyName: K) {
     if (this.has(dependencyName)) {
       return this._resources.get(dependencyName).resolve();
     }
 
-    throw new Error(`Can't find dependency '${dependencyName}' for '${resourceName}' resource`);
+    throw new Error(`Can't find dependency '${String(dependencyName)}' for '${String(resourceName)}' resource`);
   }
 
-  _registerResource(name: string, entity: ClassType<unknown>, isClass?: boolean) {
+  _registerResource<K extends keyof T>(name: K, entity: T[K], isClass?: boolean) {
     if (this.has(name)) {
-      throw new Error(`${name} resource is already registered in the container!`);
+      throw new Error(`${String(name)} resource is already registered in the container!`);
     }
 
     let resource;
@@ -37,19 +36,19 @@ export class IoCContainer {
     this._resources.set(name, resource);
   }
 
-  register(name: string, entity: any) {
+  register<K extends keyof T>(name: K, entity: T[K]) {
     this._registerResource(name, entity);
 
     return this;
   }
 
-  registerClass(name: string, entity: any) {
-    this._registerResource(name, entity, true);
+  registerClass<K extends keyof T>(name: K, service: T[K]) {
+    this._registerResource(name, service, true);
 
     return this;
   }
 
-  resolve(name: string) {
+  resolve<K extends keyof T>(name: K) {
     if (this.has(name)) {
       return this._resources.get(name).resolve();
     }
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 export type _Class<T>= new (...args: any[]) => T;
 
-export type ClassType<T> =  _Class<T> & {
+export type ClassType<T> = _Class<T> & {
   $inject?: string[];
 };
 
